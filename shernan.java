39637f875a5acc27b8173b157a4e55418c262c93
DeezNutsxz <163973542+DeezNutsxz@users.noreply.github.com>
2024-11-06 23:15:28 +0800

    wasdas

---
+++ shernan.java
@@ -0,0 +1,31 @@
+package main;
+
+import java.util.*;
+
+public class Lab{
+    public static void main(String args[]){
+
+        int s; 
+        double x; 
+        String name;
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Name: ");
+        name = s.nextLine;
+
+        System.out.print("Age: ");
+        s = s.nextInt;
+
+        System.out.print("Weight: ");
+        x = nextDoube;
+
+        System.out.println("");
+
+            System.out.println("Your Name is " + name);
+            System.out.println("Your Age are " + s);
+            System.out.println("Your Weight are heavy " + x);
+
+
+    }
+}
